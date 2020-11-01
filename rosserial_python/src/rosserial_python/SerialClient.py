9086b4eac62245d61f9ca497088265b7d57258e4
giotherobot <giotherobot@gmail.com>
2020-11-01 17:23:28 +0100

    Solved serial_node.py failure on disconnection
    serial_node.py failed when the connection was lost because of the non-existent function flushOutput(), we define it to solve the problem.

--- rosserial_python/src/rosserial_python/SerialClient.py
+++ rosserial_python/src/rosserial_python/SerialClient.py
@@ -279,6 +279,9 @@ class RosSerialServer:
     def flushInput(self):
         pass
 
+    def flushOutput(self):
+        pass
+
     def write(self, data):
         if not self.isConnected:
             return
